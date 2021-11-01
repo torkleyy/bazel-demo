1b0ec7bc9328b6c9c87c14f28c18d86d3e7e5acd
Thomas Schaller <me@torkleyy.com>
2021-11-01 08:13:54 +0100

    Give up on Rust (on Windows)

--- WORKSPACE
+++ WORKSPACE
@@ -1,5 +1,1 @@
 workspace(name = "bazel-demo")
-
-#load("@bazel_tools//tools/build_defs/repo:http.bzl", "http_archive")
-
-# Rust
--- rust-binary/src/main.rs
+++
@@ -1,9 +0,0 @@
-use rand::random;
-
-
-fn main() {
-    println!("Hello, Bazel from Rust!");
-    let nr: u8 = random();
-
-    println!("Here, a random number: {}", nr);
-}
